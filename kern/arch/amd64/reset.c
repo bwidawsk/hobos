a345c61ac6ee37193880dd4261f8e6d67df6760c
Ben Widawsky <widawsky@gmail.com>
2010-10-18 19:45:14 -0700

    Adding triple fault reset mechanism

--- kern/arch/amd64/reset.c
+++ kern/arch/amd64/reset.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 #include <bs_commands.h>
+#include "idt_amd64.h"
+#include "idt_common.h"
 #include "inlined_asm_amd64.h"
 
 static void *
@@ -13,5 +15,19 @@ reset_cmd_help() {
 	printf("Reset the damn thing\n");
 }
 
+static void *
+zero_length_idt_reset() {
+	struct idt_descriptor idtr = {
+		.limit = 0,
+		.base = 0
+	};
+	lidt((void *)&idtr);
+	__asm__ volatile ("int $0");
+	
+}
 
+#ifdef RESET_WITH_8042
 BS_COMMAND_DECLARE(reset, keyboard_controller_reset, reset_cmd_help);
+#else
+BS_COMMAND_DECLARE(reset, zero_length_idt_reset, reset_cmd_help);
+#endif
