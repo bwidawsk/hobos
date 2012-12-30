278eea2c48390d873b3a8e797586b32aae223ab3
Ben Widawsky <ben@bwidawsk.net>
2012-12-29 18:32:29 -0800

    printf: warning fix
    didn't match kvprintf prototype properly. Fixed with a little stupid
    helper.

--- kern/lib/printf.c
+++ kern/lib/printf.c
@@ -1,13 +1,19 @@
 #include <stdarg.h>
 #include <console.h>
 
+extern int kvprintf(char const *fmt, void (*func)(int, void*), void *arg, int radix, va_list ap);
+static void putc_helper(int c, void *arg)
+{
+	console_putc(c);
+}
+
 int
 printf(const char *fmt, ...) {
 	va_list ap;
 	int retval;
 
 	va_start(ap, fmt);
-	retval = kvprintf(fmt, console_putc, 0, 10, ap);
+	retval = kvprintf(fmt, putc_helper, 0, 10, ap);
 	va_end(ap);
 
 	return (retval);
