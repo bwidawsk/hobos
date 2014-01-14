5315f9fe76ced29e5bb0b440b0b15ec8b0061c84
Ben Widawsky <ben@bwidawsk.net>
2014-01-14 09:10:07 -0800

    partition: Store the partition table in the block device
    This will provide a cheap lookup later if we want to read the partition
    table.
    
    TODO: Support non legacy style partition tables
    
    Signed-off-by: Ben Widawsky <ben@bwidawsk.net>

--- kern/dev/block/block.h
+++ kern/dev/block/block.h
@@ -1,12 +1,31 @@
 #ifndef __BLOCK_H__
 #define __BLOCK_H__
 
+/* Thanks to life, there are multiple ways in which a partition can be defined,
+ * legacy PC for instance uses one thing, GPT another. Since the details of these
+ * are mostly uninteresting, we use an abstraction to define the fields we actually
+ * care about
+ */
+struct block_partition {
+	uint64_t first_block;
+};
+
+static inline void init_block_partition(struct block_partition *bpart,
+										uint64_t start)
+{
+	bpart->first_block = start;
+}
+
 struct block_device {
 	struct device base;
 	uint32_t block_size;
-	int (*read_block)(struct block_device *dev, uint64_t lba, const void *buf, uint32_t count);
-	int (*write_block)(struct block_device *dev, uint64_t lba, const void *buf, uint32_t count);
+	int (*read_block)(struct block_device *dev, uint64_t lba, const void *buf,
+					  uint32_t count);
+	int (*write_block)(struct block_device *dev, uint64_t lba, const void *buf,
+					   uint32_t count);
 	// TODO: strategy like function
+
+	struct block_partition **partition_table;
 };
 
 #define BLKDEV_FROM_DEV(dev) \
--- kern/dev/generic/partition.c
+++ kern/dev/generic/partition.c
@@ -2,26 +2,33 @@
 #include <device.h>
 #include <dev/block/block.h>
 #include <init_funcs.h>
-
-struct master_boot_record temp_mbr;
+#include <mm/malloc.h>
 
 void load_mbr(struct block_device *block_dev)
 {
+#define MAX_PARTITIONS 4
+	struct master_boot_record temp_mbr; /* WARN: kernel stack size */
+	struct primary_partition *partition;
 	int i;
+
 	uint8_t read_amt = sizeof(temp_mbr) / block_dev->block_size;
 	if (!read_amt)
 		read_amt++;
 
 	block_dev->read_block(block_dev, 0, &temp_mbr, read_amt);
-	if (temp_mbr.sig == 0xaa55) {
+	if (temp_mbr.sig == 0xaa55)
 		printf("Found a valid MBR for device\n");
-	}
 
-	for (i = 0; i < 4; i++) {
-		struct primary_partition *partition = &temp_mbr.partitions[i];
+	block_dev->partition_table = malloc(sizeof(partition) * MAX_PARTITIONS);
+
+	for (i = 0; i < MAX_PARTITIONS; i++) {
+		partition = &temp_mbr.partitions[i];
 		if (partition->type != LINUX_PARTITION_TYPE) {
 			continue;
 		}
+
+		block_dev->partition_table[i] = malloc(sizeof(*partition));
+		init_block_partition(block_dev->partition_table[i], partition->lba_first);
 	}
 }
 
