ee03544d191591fa614b9e7209664aa3ce84e364
Ben Widawsky <ben@bwidawsk.net>
2014-05-18 19:04:08 -0400

    lib: Add a zero alloc
    Totally untested

--- kern/include/mm/malloc.h
+++ kern/include/mm/malloc.h
@@ -12,4 +12,23 @@ void simple_free(const void *addr);
 #define malloc simple_malloc
 #define free simple_free
 
+static inline void *
+zalloc(uint64_t size)
+{
+	if (!size)
+		return NULL;
+
+	void *ret = malloc(size);
+	if (!ret)
+		return ret;
+
+	//bzero(ret, size);
+	do {
+		--size;
+		((unsigned char *)ret)[size] = 0;
+	} while (size);
+
+	return ret;
+}
+
 #endif
