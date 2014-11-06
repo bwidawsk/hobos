623aa782d4e9a471c3458d68dfa3741f82df0f36
Ben Widawsky <ben@bwidawsk.net>
2014-11-05 23:14:33 -0800

    buildsystem Cleanups
    This addresses the new warning generated as a result of the use of
    imacros
    commit 1ec4cea579f72244e8cc86a9a7701c52cc796d4a
    Author: Ben Widawsky <ben@bwidawsk.net>
    Date:   Sun Oct 26 14:38:31 2014 -0700
    
        cdefs: Move OFFSET_OF to cdefs.
    
    makedepend is unable to recognize that flag, and therefore that commit
    broke proper dependency tracking of cdefs.h. For now I will keep imacros
    because cdefs.h doesn't change very often. There is a new variable
    defined called ESOTERIC_INCLUDES (can be named ESOTERIC_CFLAGS later)
    which will be ignored by makedepend.
    
    While in the build goop, I added some cleanups and comments.
    
    Also while in the goop, fix up warnings. By design, Make will spit out a
    warning if the operation failed, even if the command is preceded with a
    '-'. I find this annoying.

--- Makefile
+++ Makefile
@@ -5,14 +5,14 @@ SUBDIRS:=$(patsubst %/Makefile,%,$(wildcard */Makefile))
 
 # This represents the ideal setup with a tiled window manager
 all: boot/bootimage
-	-rm mon ser
+	-@rm -f mon ser
 	$(TERM) -title "HOBOS SERIAL PORT" -e socat UNIX-LISTEN:ser - &
 	$(TERM) -title "HOBOS QEMU MONITOR" -e socat UNIX-LISTEN:mon - &
 	sleep 1
 	$(TERM) -e qemu-system-x86_64 bootimage/disk_image -serial unix:ser -monitor unix:mon -debugcon stdio -bios $(BIOS)
 
 debug: boot/bootimage
-	-rm mon ser
+	-@rm -f mon ser
 	$(TERM) -title "HOBOS SERIAL PORT" -e socat UNIX-LISTEN:ser - &
 	$(TERM) -title "HOBOS QEMU MONITOR" -e socat UNIX-LISTEN:mon - &
 	sleep 5 && $(TERM) -title "HOBOS GDB" -cd $(CURDIR)/kern -e gdb --command=debug_kernel.gdb kernel &
--- kern/hobo_kernel.mk
+++ kern/hobo_kernel.mk
@@ -1,19 +1,19 @@
-CFLAGS+=$(INCLUDES)
-
+# This file is to be included by any standard subdirectory within the kernel
+# source tree. It will generate dependencies using makedepend. This is
+# accomplished by having the Makefile in the directory define special variables
+# (see below for the special variables since comments always get stale).
 OBJS=$(subst .c,.o,$(abspath $(SOURCES)))
 
-$(OUTPUT) : $(OBJS) depend
+$(OUTPUT): $(OBJS) depend
 	$(LD) $(LDFLAGS) -r $(OBJS) -o $@
 
-.PHONY : clean
-clean :
-	-rm $(OUTPUT) $(OBJS)
-	-rm depend
-	-rm depend.bak
+.PHONY: clean
+clean:
+	-@rm -f $(OUTPUT) $(OBJS) depend depend.bak
 
 depend: $(SOURCES)
-	rm -f depend
-	touch depend
-	$(MAKEDEPEND) -fdepend -Y $(INCLUDES) $^
+	-@rm -f depend
+	@touch depend # Is this even necessary?
+	-@$(MAKEDEPEND) -fdepend -Y $(INCLUDES) -- $(ESOTERIC_INCLUDES) -- $^
 
 -include depend
