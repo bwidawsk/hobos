b15ba6f7dd19bf7e29e03bb7d3689d8d2effa888
Ben Widawsky <ben@bwidawsk.net>
2014-11-08 17:39:41 -0800

    Makefile: Start on the build pretty printer
    There is still more to do, but this handles most things.

--- kern/hobo_kernel.mk
+++ kern/hobo_kernel.mk
@@ -3,9 +3,24 @@
 # accomplished by having the Makefile in the directory define special variables
 # (see below for the special variables since comments always get stale).
 OBJS=$(subst .c,.o,$(abspath $(SOURCES)))
+# This is based on what Automake does for verbosity.
+#V=0
+v_CC_0=@echo "  CC     $(subst $(ROOTDIR)/, , $<)"; $(CC)
+v_CC_1=$(CC)
+v_CC_=$(v_CC_0)
+_CC=$(v_CC_$(V))
+
+v_LD_0=@$(LD)
+v_LD_1=@echo "  LD     $(subst $(ROOTDIR)/, , $<)"; $(LD)
+v_LD_2=$(LD)
+v_LD_=$(v_LD_0)
+_LD=$(v_LD_$(V))
+
+%.o: %.c
+	$(_CC) $(CFLAGS) -c -o $@ $<
 
 $(OUTPUT): $(OBJS) depend
-	$(LD) $(LDFLAGS) -r $(OBJS) -o $@
+	$(_LD) $(LDFLAGS) -r $(OBJS) -o $@
 
 .PHONY: clean
 clean:
