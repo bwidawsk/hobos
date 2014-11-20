05b3e856b66bc8fbf77adc0b27ca3e4e6df979f5
Ben Widawsky <ben@bwidawsk.net>
2014-11-20 09:03:28 -0800

    arch_memset (amd64): Make it work
    Clobbers:
    The GCC info doc for inline assembly explicitly states:
    > Variables declared to live in specific registers (*note Explicit Reg Vars::),
    > and used as asm input or output operands must have no part mentioned in the
    > clobber description.  There is no way for you to specify that an input operand
    > is modified without also specifying it as an output operand.  Note that if all
    > the output operands you specify are for this purpose (and hence unused), you
    > then also need to specify 'volatile' for the 'asm' construct, as described
    > below, to prevent GCC from deleting the 'asm' statement as unused.
    
    Since I was defaulting to volatile anyway, this should just work. I also removed
    the const qualifiers since the values modified by the stos instruction.
    
    Outputs:
    I don't know what I'm doing with inline asm, and the info pages for GCC [on my
    system] doesn't have x86-64 information. Apparently you need outs. Not worth
    investigating further.

--- kern/arch/amd64/libc_memfuncs.c
+++ kern/arch/amd64/libc_memfuncs.c
@@ -2,22 +2,23 @@ void arch_memset(void *s, int c, uint64_t n)
 {
 	KWARN_NOW(c & ~0xff);
 	c &= 0xff;
-	const uint64_t qwords = n >> 3;
-	const uint64_t bytes = n & 0x7;
+	uint64_t qwords = n >> 3;
+	uint64_t bytes = n & 0x7;
 	uint64_t byte = 0x0101010101010101;
+	uint64_t cx, di;
 	byte *= c;
 
 	__asm__ volatile("rep stosq"
-					 :
-					 : "rax" (byte), "rcx" (qwords), "rdi" (s)
-					 : "rcx", "rdi", "memory");
+					 : "=&c" (cx), "=&D" (di)
+					 : "a" (byte), "0" (qwords), "1" (s)
+					 : "memory");
 	if (!bytes)
 		return;
 
 	s += qwords;
 
 	__asm__ volatile("rep stosb"
-					 :
-					 : "rax" (c), "rcx" (bytes), "rdi" (s)
-					 : "rcx", "rdi", "memory");
+					 : "=&c" (cx), "=&D" (di)
+					 : "a" ((uint8_t)c), "0" (bytes), "1" (s)
+					 : "memory");
 }
